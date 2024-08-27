258cc9f57b747aa16feb1e6bc30d200ef34e714b
Radovan Markovic <radovanm02@gmail.com>
2024-08-27 21:31:50 +0200

    init commit

---
+++ online-chess.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE online_chess;
+
+USE online_chess;
+
+-- Tables
+CREATE TABLE users(
+	id INT auto_increment primary key,
+    username VARCHAR(255) UNIQUE,
+    email VARCHAR(255) UNIQUE,
+    password VARCHAR(255)
+);
+
+create table user_info (
+	user_id int,
+	user_rank enum('beginner', 'intermediate','advanced', 'expert') default 'beginner',
+	user_points int default 1000,
+	key userID(user_id),
+	constraint userID foreign key(user_id) references users(id) on delete cascade
+);
+
+
+-- Procedures
+delimiter $$
+create procedure createUser(	
+	in _username varchar(255),
+    in _email varchar(255),
+    in _password varchar(255)
+)
+begin
+	declare userId int;
+    
+    insert into users(username,email,password) values (_username,_email,_password);
+    select id into userId from users where username = _username;
+    insert into user_info(user_id) value(userId);
+end $$ 
+delimiter ;
+
+
+-- Queries
+select * from users;
+
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const db = require("./config/db");
+const path = require("path");
+const cookieParser = require('cookie-parser');
+const redisClient = require("./config/redis");
+
+dotenv.config();
+
+//Routes
+const viewRoutes = require("./routes/views");
+const userRoutes = require("./routes/api/user");
+
+const app = express();
+
+db.connect((err) =>{
+    if(err){
+        console.log(err);
+        process.exit(1);
+    }
+
+    console.log("Conecting to MySQL database");
+})
+
+//postavljanje aplikacije da se sve povezuje preko ejs-a, takodje i sa public zbog svi css,js fajlova
+app.use(cookieParser("secret"));
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//ruta za posecivanje stranice, odmah ce da ode na index iz views foldera
+app.use("/", viewRoutes);
+app.use("/api", userRoutes);
+
+//uzmemo procitamo iz dotenv varijablu PORT i pokupimo njenu vrednost, ako ne postoji onda se stavlja na 5000
+const PORT = parseInt(process.env.PORT) || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`);
+})
